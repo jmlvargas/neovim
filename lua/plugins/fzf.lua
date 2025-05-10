4fc7d4563799aa89e24bbeebb6ccc27f0e76615f
jmlvargas <jmlvargas@gmail.com>
2025-05-10 13:22:02 -0600

    fzf and project.nvim (deprecated)

---
+++ lua/plugins/fzf.lua
@@ -0,0 +1,80 @@
+return {
+  "ibhagwan/fzf-lua",
+  -- optional for icon support
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- or if using mini.icons/mini.nvim
+  -- dependencies = { "echasnovski/mini.icons" },
+  opts = {},
+  keys = {
+    { 
+      "<leader>ff",
+      function() require('fzf-lua').files() end,
+      desc = "Find Files"
+    },
+    { 
+      "<leader>fg",
+      function() require('fzf-lua').live_grep() end,
+      desc = "Find by Grep"
+    },
+    { 
+      "<leader>fc",
+      function() require('fzf-lua').files({cwd=vim.fn.stdpath("config")}) end,
+      desc = "Find Configuration Files"
+    },
+    { 
+      "<leader>fi",
+      function() require('fzf-lua').builtin() end,
+      desc = "Find Built-in Fuzzy Finders"
+    },
+    { 
+      "<leader>fh",
+      function() require('fzf-lua').helptags() end,
+      desc = "Find Help"
+    },
+    { 
+      "<leader>fk",
+      function() require('fzf-lua').keymaps() end,
+      desc = "Find Keymaps"
+    },
+    { 
+      "<leader>fk",
+      function() require('fzf-lua').keymaps() end,
+      desc = "Find Keymaps"
+    },
+    { 
+      "<leader>fw",
+      function() require('fzf-lua').grep_cword() end,
+      desc = "Find Current Word"
+    }, 
+    { 
+      "<leader>fW",
+      function() require('fzf-lua').grep_cWORD() end,
+      desc = "Find Current WORD"
+    },
+    { 
+      "<leader>fd",
+      function() require('fzf-lua').diagnostics_document() end,
+      desc = "Find Diagnostics"
+    },
+    { 
+      "<leader>fr",
+      function() require('fzf-lua').resume() end,
+      desc = "Find Resume"
+    },
+    { 
+      "<leader>fo",
+      function() require('fzf-lua').oldfiles() end,
+      desc = "Find Old Files"
+    },
+    { 
+      "<leader><leader>",
+      function() require('fzf-lua').buffers() end,
+      desc = "Find Buffers"
+    },
+    { 
+      "<leader>/",
+      function() require('fzf-lua').lgrep_curbuf() end,
+      desc = "Find in Current Buffer"
+    }
+  }
+}
