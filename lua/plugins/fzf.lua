f13d7c5777c2490ee94ff55d9e6d4368a5a74b84
jmlvargas <jmlvargas@gmail.com>
2025-05-10 23:45:37 -0600

    PyRight

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -6,75 +6,103 @@ return {
   -- dependencies = { "echasnovski/mini.icons" },
   opts = {},
   keys = {
-    { 
+    {
       "<leader>ff",
-      function() require('fzf-lua').files() end,
-      desc = "Find Files"
+      function()
+        require("fzf-lua").files()
+      end,
+      desc = "Find Files",
     },
-    { 
+    {
       "<leader>fg",
-      function() require('fzf-lua').live_grep() end,
-      desc = "Find by Grep"
+      function()
+        require("fzf-lua").live_grep()
+      end,
+      desc = "Find by Grep",
     },
-    { 
+    {
       "<leader>fc",
-      function() require('fzf-lua').files({cwd=vim.fn.stdpath("config")}) end,
-      desc = "Find Configuration Files"
+      function()
+        require("fzf-lua").files({ cwd = vim.fn.stdpath("config") })
+      end,
+      desc = "Find Configuration Files",
     },
-    { 
+    {
       "<leader>fi",
-      function() require('fzf-lua').builtin() end,
-      desc = "Find Built-in Fuzzy Finders"
+      function()
+        require("fzf-lua").builtin()
+      end,
+      desc = "Find Built-in Fuzzy Finders",
     },
-    { 
+    {
       "<leader>fh",
-      function() require('fzf-lua').helptags() end,
-      desc = "Find Help"
+      function()
+        require("fzf-lua").helptags()
+      end,
+      desc = "Find Help",
     },
-    { 
+    {
       "<leader>fk",
-      function() require('fzf-lua').keymaps() end,
-      desc = "Find Keymaps"
+      function()
+        require("fzf-lua").keymaps()
+      end,
+      desc = "Find Keymaps",
     },
-    { 
+    {
       "<leader>fk",
-      function() require('fzf-lua').keymaps() end,
-      desc = "Find Keymaps"
+      function()
+        require("fzf-lua").keymaps()
+      end,
+      desc = "Find Keymaps",
     },
-    { 
+    {
       "<leader>fw",
-      function() require('fzf-lua').grep_cword() end,
-      desc = "Find Current Word"
-    }, 
-    { 
+      function()
+        require("fzf-lua").grep_cword()
+      end,
+      desc = "Find Current Word",
+    },
+    {
       "<leader>fW",
-      function() require('fzf-lua').grep_cWORD() end,
-      desc = "Find Current WORD"
+      function()
+        require("fzf-lua").grep_cWORD()
+      end,
+      desc = "Find Current WORD",
     },
-    { 
+    {
       "<leader>fd",
-      function() require('fzf-lua').diagnostics_document() end,
-      desc = "Find Diagnostics"
+      function()
+        require("fzf-lua").diagnostics_document()
+      end,
+      desc = "Find Diagnostics",
     },
-    { 
+    {
       "<leader>fr",
-      function() require('fzf-lua').resume() end,
-      desc = "Find Resume"
+      function()
+        require("fzf-lua").resume()
+      end,
+      desc = "Find Resume",
     },
-    { 
+    {
       "<leader>fo",
-      function() require('fzf-lua').oldfiles() end,
-      desc = "Find Old Files"
+      function()
+        require("fzf-lua").oldfiles()
+      end,
+      desc = "Find Old Files",
     },
-    { 
+    {
       "<leader><leader>",
-      function() require('fzf-lua').buffers() end,
-      desc = "Find Buffers"
+      function()
+        require("fzf-lua").buffers()
+      end,
+      desc = "Find Buffers",
     },
-    { 
+    {
       "<leader>/",
-      function() require('fzf-lua').lgrep_curbuf() end,
-      desc = "Find in Current Buffer"
-    }
-  }
+      function()
+        require("fzf-lua").lgrep_curbuf()
+      end,
+      desc = "Find in Current Buffer",
+    },
+  },
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -203,7 +203,7 @@ return {
       bashls = {},
       -- clangd = {},
       -- gopls = {},
-      -- pyright = {},
+      pyright = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
