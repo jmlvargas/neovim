d24a0e84369295ef96d771a918dea0feb69d5f9d
jmlvargas <jmlvargas@gmail.com>
2025-05-12 19:35:04 -0600

    Use space for incremental select.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,30 +1,41 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function () 
+  config = function()
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir",
-        "heex", "javascript", "html", "python", "markdown" },
+      ensure_installed = {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "elixir",
+        "heex",
+        "javascript",
+        "html",
+        "python",
+        "markdown",
+      },
       sync_install = false,
       highlight = { enable = true },
-      indent = { enable = true }, 
+      indent = { enable = true },
       -- Automatically install missing parsers when entering buffer
       auto_install = false,
 
       -- List of parsers to ignore installing (or "all')
-      ignore_install = { },
+      ignore_install = {},
 
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "<Enter>", --"gnn", -- set to `false` to disable one of the mappings
-          node_incremental = "<Enter>", --"grn",
+          init_selection = "<space>", --"gnn", -- set to `false` to disable one of the mappings
+          node_incremental = "<space>", --"grn",
           scope_incremental = false, --"grc",
           node_decremental = "<Backspace>", -- "grm",
         },
       },
     })
-  end
+  end,
 }
