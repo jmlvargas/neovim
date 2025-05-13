a5d71379aade878e39b862ebd068a7ff2f7bfa5d
jmlvargas <jmlvargas@gmail.com>
2025-05-12 22:44:28 -0600

    Transparent background

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,14 @@
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000,
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
     enabled = true,
-    config = function ()
+    config = function()
       require("catppuccin").setup({
+        transparent_background = true,
       })
-      vim.cmd('colorscheme catppuccin')
-    end
+      vim.cmd("colorscheme catppuccin")
+    end,
   },
 }
